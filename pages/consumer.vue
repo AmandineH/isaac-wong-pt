41e527586dd69d97648ff2c962c2933f158b0f5f
angeliaL <angelia@realestateanalytics.sg>
2021-10-29 12:12:22 +0800

    consumer page

--- pages/consumer.vue
+++ pages/consumer.vue
@@ -13,6 +13,98 @@
 
       <h1 class="supheader-1 relative z-10">Consumer</h1>
     </div>
+
+  <div class="max-w-full">
+    <div class="flex flex-col items-top">
+      <div class="m-20px">
+        <img src="@/assets/8-prop.png" alt="8-prop" />
+          <div class="mt-20px mb-20px">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Aenean ac tellus id dui semper aliquam. Maecenas vitae felis
+            et orci molestie sagittis. Mauris at ligula eu est porta porta
+            at eu est. Vestibulum tincidunt nunc metus, nec scelerisque
+            ligula vestibulum a. Curabitur facilisis vehicula nibh sit
+            amet aliquet
+            </p>
+          </div>
+
+         <div class="md:flex"> 
+          <div class="mt-20px mb-20px">
+            <div class="mb-20px">
+              <h1 class="text-blue-191F6B text-left title-1">8PROP 360</h1>
+            </div>
+                <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Aenean ac tellus id dui semper aliquam. Maecenas vitae felis
+              et orci molestie sagittis. Mauris at ligula eu est porta porta
+              at eu est. Vestibulum tincidunt nunc metus, nec scelerisque
+              ligula vestibulum a. Curabitur facilisis vehicula nibh sit
+              amet aliquet
+                </p>
+          </div>
+          
+          <div class="mt-20px mb-20px">
+            <div class="mb-20px">
+              <h1 class="text-blue-191F6B text-left title-1">8PROP Concierge</h1>
+            </div>
+                <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Aenean ac tellus id dui semper aliquam. Maecenas vitae felis
+              et orci molestie sagittis. Mauris at ligula eu est porta porta
+              at eu est. Vestibulum tincidunt nunc metus, nec scelerisque
+              ligula vestibulum a. Curabitur facilisis vehicula nibh sit
+              amet aliquet
+                </p>
+          </div>
+         </div>
+
+         <div class="md:flex"> 
+          <div class="mt-20px mb-20px">
+            <div class="mb-20px">
+              <h1 class="text-blue-191F6B text-left title-1">8PROP Valuation</h1>
+            </div>
+                <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Aenean ac tellus id dui semper aliquam. Maecenas vitae felis
+              et orci molestie sagittis. Mauris at ligula eu est porta porta
+              at eu est. Vestibulum tincidunt nunc metus, nec scelerisque
+              ligula vestibulum a. Curabitur facilisis vehicula nibh sit
+              amet aliquet
+                </p>
+          </div>
+
+          <div class="mt-20px mb-20px">
+            <div class="mb-20px">
+              <h1 class="text-blue-191F6B text-left title-1">8PROP Marketplace</h1>
+            </div>
+                <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Aenean ac tellus id dui semper aliquam. Maecenas vitae felis
+              et orci molestie sagittis. Mauris at ligula eu est porta porta
+              at eu est. Vestibulum tincidunt nunc metus, nec scelerisque
+              ligula vestibulum a. Curabitur facilisis vehicula nibh sit
+              amet aliquet
+                </p>
+          </div>
+        </div>
+
+
+          
+    </div>
+
+
+
+
+
+  </div>
+</div>
+
+
+
+
+
+
   </div>
 </template>
 
